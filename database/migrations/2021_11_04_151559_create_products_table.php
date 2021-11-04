7cbcd30c1b79706ee6d1caffcdface18c0cc0edd
Bui Thanh Dat <datbtth2011018@fpt.edu.vn>
2021-11-04 23:22:09 +0700

    Tao Migrate

---
+++ database/migrations/2021_11_04_151559_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->double('price');
+            $table->unsignedBigInteger('category_id');
+            $table->text('description');
+            $table->text('detail');
+            $table->text('thumbnail');
+            $table->tinyInteger('status')->default(1);
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ database/migrations/2021_11_04_151824_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Enums\OrderStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->boolean('check_out');
+            $table->double('total_price');
+            $table->string('ship_name');
+            $table->string('ship_phone');
+            $table->string('ship_email');
+            $table->string('ship_address');
+            $table->string('ship_note')->nullable();
+            $table->tinyInteger('ship_status')->default(OrderStatus::Waiting);
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
