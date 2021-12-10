9aec2433af220fd4ef00731a0e0f0de9febeecad
Nguyen Tung Bach <bachntth2010055@fpt.edu.vn>
2021-12-10 19:57:24 +0700

    Bach seed

--- database/migrations/2021_11_04_151824_create_orders_table.php
+++ database/migrations/2021_11_04_151824_create_orders_table.php
@@ -15,7 +15,7 @@ class CreateOrdersTable extends Migration
     public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->id();
+            $table->id()->default(20);
             $table->boolean('check_out');
             $table->double('total_price');
             $table->string('ship_name');
