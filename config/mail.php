ae1b5e496a699e925733ec660c349878cb1bd831
NguyenHoang1303 <78024759+NguyenHoang1303@users.noreply.github.com>
2021-12-11 01:13:48 +0700

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -79,14 +79,6 @@ return [
             ],
         ],
     ],
-    
-    'stream' => [
-        'ssl' => [
-            'allow_self_signed' => true,
-            'verify_peer'       => false,
-            'verify_peer_name'  => false,
-        ],
-    ],
 
     /*
     |--------------------------------------------------------------------------
