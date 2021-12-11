20dab6d9c97d5da9171a5fb5bdefde5085e3d118
Nguyen Tung Bach <bachntth2010055@fpt.edu.vn>
2021-12-11 12:42:28 +0700

    Sửa lỗi admin, authcheck, mail

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -29,7 +29,7 @@ class DashboardController extends Controller
 
         $quantityProductQuery = "
         SELECT SUM(quantity) as total_Quantity, products.name as name_Product, product_id FROM order_details
-        LEFT JOIN products ON order_details.product_id = products.id
+        LEFT JOIN products USING order_details.product_id
         GROUP BY  products.name ORDER BY total_Quantity DESC";
         $resultTotalPrice = DB::select(DB::raw($quantityProductQuery));
 
