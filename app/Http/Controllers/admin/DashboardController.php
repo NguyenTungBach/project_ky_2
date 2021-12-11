67082a75501cf4c8f36b8473d07536f174c6b1da
Nguyen Tung Bach <bachntth2010055@fpt.edu.vn>
2021-12-11 13:00:43 +0700

    Sửa lỗi admin, authcheck, mail

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -28,19 +28,19 @@ class DashboardController extends Controller
 
 
         $quantityProductQuery = "
-        SELECT SUM(quantity) as total_Quantity, products.name as name_Product, product_id
+        SELECT SUM(quantity) as total_quantity, products.name as name_product, product_id
         FROM order_details
         LEFT JOIN products ON order_details.product_id = products.id
-        GROUP BY order_details.product_id,products.name ORDER BY total_Quantity DESC";
+        GROUP BY order_details.product_id,products.name ORDER BY total_quantity DESC";
         $resultTotalPrice = DB::select(DB::raw($quantityProductQuery));
 
         $dataQuantity ='';
         $quantityAnother = 0;
         for($i = 0; $i < sizeof($resultTotalPrice); $i++){
             if($i < 10){
-                $dataQuantity .= "['".$resultTotalPrice[$i]->name_Product."', ".$resultTotalPrice[$i]->total_Quantity.",".$resultTotalPrice[$i]->product_id."],";
+                $dataQuantity .= "['".$resultTotalPrice[$i]->name_product."', ".$resultTotalPrice[$i]->total_quantity.",".$resultTotalPrice[$i]->product_id."],";
             }else{
-                $quantityAnother += $resultTotalPrice[$i]->total_Quantity;
+                $quantityAnother += $resultTotalPrice[$i]->total_quantity;
             }
         }
         $dataQuantity .= "['Sản phẩm còn lại', ".$quantityAnother.",'none'],";
