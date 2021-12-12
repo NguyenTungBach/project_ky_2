1cbe57533db5b418fe456a730524b568b00c1f51
Nguyen Tung Bach <bachntth2010055@fpt.edu.vn>
2021-12-12 10:54:13 +0700

    Bach

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['https://web-service-test-ky-2.herokuapp.com'],
 
     'allowed_headers' => ['*'],
 
